4f7a83df024aa70d440569b49d826a230128cf9b
kywpcm <kywpcm@gmail.com>
2017-09-22 01:31:33 +0900

    커밋...

---
+++ KywpcmDotXyz/assets/js/smoothly-scrolling.js
@@ -0,0 +1,32 @@
+/*!
+ * Start Bootstrap - Agnecy Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    // 2017.09.16 modify (KWONYW)
+    console.debug("테스트!@@#!#");
+    console.debug($(this).attr('class'));
+    if ($(this).attr('class') !== '.dropdown-toggle') {
+        $('.navbar-toggle:visible').click();
+    }
+    // $('.navbar-toggle:visible').click();
+});
